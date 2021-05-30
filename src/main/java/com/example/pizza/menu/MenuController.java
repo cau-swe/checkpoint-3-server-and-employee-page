cc5be5d3a99d2ba375b0704b769646194342fa83
Leem <wjdxor6346@naver.com>
2021-05-30 16:01:25 +0900

    피자 메뉴 단건 조회

--- src/main/java/com/example/pizza/menu/MenuController.java
+++ src/main/java/com/example/pizza/menu/MenuController.java
@@ -3,9 +3,11 @@ package com.example.pizza.menu;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
@@ -14,10 +16,20 @@ public class MenuController {
     private final MenuDatabaseConnection menuDatabaseConnection;
 
     @GetMapping("/menus")
-    ResponseEntity<List<MenuResponse>> findAll() {
+    public ResponseEntity<List<MenuResponse>> findAll() {
         List<Menu> menus = menuDatabaseConnection.findAll();
         return ResponseEntity.ok(menus.stream()
                 .map(menu -> new MenuResponse(menu.getId(), menu.getName(), menu.getDescription(), menu.getPrice()))
                 .collect(Collectors.toList()));
+    }
+
+    @GetMapping("/menus/{id}")
+    public ResponseEntity<MenuResponse> findPizza(@PathVariable Long id) {
+        Optional<Menu> findMenu = menuDatabaseConnection.findById(id);
+        if (findMenu.isPresent()) {
+            Menu menu = findMenu.get();
+            return ResponseEntity.ok(new MenuResponse(menu.getId(), menu.getName(), menu.getDescription(), menu.getPrice()));
+        }
+        return ResponseEntity.badRequest().build();
     }
 }
