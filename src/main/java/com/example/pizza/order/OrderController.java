f98276cab303ac8beaba9847527cb9f11d6cf25a
Leem <98qkrwjdal@naver.com>
2021-05-31 04:56:16 +0900

    주문 상태 변경 기능

--- src/main/java/com/example/pizza/order/OrderController.java
+++ src/main/java/com/example/pizza/order/OrderController.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.Optional;
 
@@ -16,6 +18,18 @@ public class OrderController {
     public String findOrder(@PathVariable Long id) {
         Optional<Order> find = orderDatabaseConnection.findById(id);
         if (find.isPresent()) {
+            return "order-detail";
+        }
+        return "not-found";
+    }
+
+    @PostMapping("/orders/{id}/state")
+    public String changeOrderState(@PathVariable Long id, @RequestBody String orderState) {
+        Optional<Order> find = orderDatabaseConnection.findById(id);
+        if (find.isPresent()) {
+            Order order = find.get();
+            order.changeState(orderState);
+            orderDatabaseConnection.save(order);
             return "order-detail";
         }
         return "not-found";
