3a96387c30cea64794d11a8994b16011840f6a9f
Jungmi <lastpuzly@gmail.com>
2021-05-31 21:02:17 +0900

    feat: 주문 상태 변경 페이지 연결

--- src/main/java/com/example/pizza/order/OrderController.java
+++ src/main/java/com/example/pizza/order/OrderController.java
@@ -2,13 +2,14 @@ package com.example.pizza.order;
 
 import com.example.pizza.menu.cart.Cart;
 import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Optional;
 
 @RequiredArgsConstructor
@@ -21,34 +22,36 @@ public class OrderController {
         Optional<Order> find = orderDatabaseConnection.findById(id);
         if (find.isPresent()) {
             Order order = find.get();
-            model.addAttribute("order", order);
-            model.addAttribute("carts", order.getCarts());
-            model.addAttribute("user", order.getCarts().get(0).getUser());
-
-            int totalPrice = order.getCarts()
-                    .stream()
-                    .map(Cart::getPrice)
-                    .mapToInt(Integer::intValue)
-                    .sum();
-
-            model.addAttribute("totalPrice", totalPrice);
-            return "order-detail";
+            return renderOrderDetail(model, order);
         }
         return "not-found";
     }
 
     @PostMapping("/orders/{id}/state")
-    public String changeOrderState(@PathVariable Long id, @RequestBody String orderState, Model model) {
+    public String changeOrderState(@PathVariable Long id, HttpServletRequest request, Model model) {
         Optional<Order> find = orderDatabaseConnection.findById(id);
         if (find.isPresent()) {
             Order order = find.get();
-            order.changeState(orderState);
+            order.changeState(request.getParameter("state"));
             Order saved = orderDatabaseConnection.save(order);
-            model.addAttribute("order", saved);
-            model.addAttribute("carts", order.getCarts());
-            model.addAttribute("user", order.getCarts().get(0).getUser());
-            return "order-detail";
+            return renderOrderDetail(model, saved);
         }
         return "not-found";
+    }
+
+    @NotNull
+    private String renderOrderDetail(Model model, Order order) {
+        model.addAttribute("order", order);
+        model.addAttribute("carts", order.getCarts());
+        model.addAttribute("user", order.getCarts().get(0).getUser());
+
+        int totalPrice = order.getCarts()
+                .stream()
+                .map(Cart::getPrice)
+                .mapToInt(Integer::intValue)
+                .sum();
+
+        model.addAttribute("totalPrice", totalPrice);
+        return "order-detail";
     }
 }
